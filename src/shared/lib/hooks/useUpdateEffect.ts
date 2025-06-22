e0a0c63f19d90638b39a218434377d8fefa6b2aa
Dmitriy Martynenko <d.martynenko@tg.dunice.net>
2025-06-22 14:47:49 +0300

    Added types

--- src/shared/lib/hooks/useUpdateEffect.ts
+++ src/shared/lib/hooks/useUpdateEffect.ts
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from 'react';
 
-export const useUpdateEffect = (callback, deps) => {
+export const useUpdateEffect = (callback: any, deps: any) => {
   const ref = useRef<boolean>(false);
 
   useEffect(() => {
