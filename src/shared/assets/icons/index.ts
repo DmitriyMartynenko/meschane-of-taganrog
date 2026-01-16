df41514ca62ead6a0f78509719aae50f22639b25
JosephPerson <thejosephperson@mail.ru>
2026-01-16 22:23:27 +0300

    chore(promo-background): move promo-background.png to shared/assets

--- src/shared/assets/icons/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Logo } from './logo.svg';
-export { default as Quotes } from './quotes.svg';
--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,1 +1,3 @@
-export * from './icons';
+export { default as Logo } from './logo.png';
+export { default as Quotes } from './quotes.svg';
+export { default as promoBackground } from './promo-background.png';
