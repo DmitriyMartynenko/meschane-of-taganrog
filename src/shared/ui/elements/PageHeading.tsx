60095491a3ec19a5cf523ab7964655ae03dc84be
JosephPerson <thejosephperson@mail.ru>
2026-01-16 22:49:11 +0300

    refactor(page-heading): refactor PageHeading into composable subcomponents

--- src/shared/ui/elements/PageHeading.tsx
+++ src/shared/ui/elements/PageHeading.tsx
@@ -1,21 +1,36 @@
+import { ComponentProps } from 'react';
+
 import { cn } from '@/shared/lib';
 
-type PageHeadingProps = {
-  className?: string;
-  highlight: string;
-  caption: string;
-};
-
-export const PageHeading = (props: PageHeadingProps) => {
-  const { className = '', highlight, caption } = props;
+export const PageHeading = (props: ComponentProps<'h1'>) => {
+  const { className, children, ...restProps } = props;
 
   return (
     <h1
       className={cn('text-shadow-promo-title text-center font-chibola text-5xl/tight', className)}
+      {...restProps}
     >
-      <span className="text-[4rem] text-secondary">{highlight}</span>
-      <br />
-      <span className="text-foreground-secondary">{caption}</span>
+      {children}
     </h1>
+  );
+};
+
+export const PageHeadingHighlight = (props: ComponentProps<'span'>) => {
+  const { className, children, ...restProps } = props;
+
+  return (
+    <span className={cn('text-[4rem] text-secondary', className)} {...restProps}>
+      {children}
+    </span>
+  );
+};
+
+export const PageHeadingCaption = (props: ComponentProps<'span'>) => {
+  const { className, children, ...restProps } = props;
+
+  return (
+    <span className={cn('block text-foreground-secondary', className)} {...restProps}>
+      {children}
+    </span>
   );
 };
