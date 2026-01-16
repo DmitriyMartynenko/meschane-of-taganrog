d7cde26ffbf87f4dbf965ce0a049be489c714633
JosephPerson <thejosephperson@mail.ru>
2026-01-16 23:11:51 +0300

    refactor(promo-section): add PageHeading composition

--- src/app-pages/excursions/ui/PromoSection.tsx
+++ src/app-pages/excursions/ui/PromoSection.tsx
@@ -1,15 +1,24 @@
 import { ExcursionFilter } from '@/features/excursion-filtering';
 
-import { BackgroundImage, Container, PageHeading, Section } from '@/shared/ui';
-
-import promoBackground from '../assets/images/promo-background.png';
+import { promoBackground } from '@/shared/assets';
+import {
+  BackgroundImage,
+  Container,
+  PageHeading,
+  PageHeadingCaption,
+  PageHeadingHighlight,
+  Section,
+} from '@/shared/ui';
 
 export const PromoSection = () => {
   return (
     <Section className="relative flex min-h-screen overflow-hidden bg-none">
-      <BackgroundImage src={promoBackground} />
-      <Container className="relative flex-col gap-16 py-32">
-        <PageHeading highlight="Наши экскурсии —" caption="Погружение в мещанский мир Таганрога" />
+      <BackgroundImage className="scale-101 opacity-100 blur-xs" src={promoBackground} />
+      <Container className="relative flex flex-col items-center justify-center gap-16 py-32">
+        <PageHeading>
+          <PageHeadingHighlight>Наши экскурсии —</PageHeadingHighlight>
+          <PageHeadingCaption>Погружение в мещанский мир Таганрога</PageHeadingCaption>
+        </PageHeading>
         <ExcursionFilter />
       </Container>
     </Section>
