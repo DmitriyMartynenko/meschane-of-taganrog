b5b2993be53748c3cd6c47d302dab844f0f2bef9
JosephPerson <thejosephperson@mail.ru>
2026-02-04 10:46:12 +0300

    refactor(no-excursions-placeholder): add optional className prop

--- src/widgets/excursions-list/ui/NoExcursionsPlaceholder.tsx
+++ src/widgets/excursions-list/ui/NoExcursionsPlaceholder.tsx
@@ -1,8 +1,16 @@
 import Link from 'next/link';
 
-export const NoExcursionsPlaceholder = () => {
+import { cn } from '@/shared/lib';
+
+type NoExcursionsPlaceholderProps = {
+  className?: string;
+};
+
+export const NoExcursionsPlaceholder = (props: NoExcursionsPlaceholderProps) => {
+  const { className } = props;
+
   return (
-    <div className="flex flex-col items-center justify-center gap-4 pb-8 text-center">
+    <div className={cn('flex flex-col items-center justify-center gap-4 text-center', className)}>
       <h3 className="text-2xl font-bold text-foreground-primary">
         Подходящих Вам экскурсий не найдено.
       </h3>
