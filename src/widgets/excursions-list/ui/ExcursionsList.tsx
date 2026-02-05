5f864c595dd399a03e4f590ed106d76391305d09
JosephPerson <thejosephperson@mail.ru>
2026-02-05 16:19:39 +0300

    fix(excursions-list): increase margin-top of ExcursionsList component

--- src/widgets/excursions-list/ui/ExcursionsList.tsx
+++ src/widgets/excursions-list/ui/ExcursionsList.tsx
@@ -5,6 +5,8 @@ import { useExcursionFiltering, useFilterStore } from '@/features/excursion-filt
 
 import { ExcursionCard } from '@/entities/excursion';
 
+import { cn } from '@/shared/lib';
+
 import { sortExcursionsByDate } from '../lib/sort-excursions-by-date';
 import { excursions } from '../model/excursions.mock';
 
@@ -15,12 +17,13 @@ import { NoExcursionsPlaceholder } from './NoExcursionsPlaceholder';
 export const ExcursionsList = () => {
   const { filteredExcursions, isPending } = useExcursionFiltering(excursions);
   const filters = useFilterStore((state) => state.filters);
+  const hasActiveFilters = useFilterStore((state) => state.hasActiveFilters);
   const openBookingDialog = useBookingDialogStore((state) => state.open);
 
   const sortedExcursions = sortExcursionsByDate(filteredExcursions, 'asc');
 
   return (
-    <div className="mt-8 flex flex-col items-center gap-8">
+    <div className={cn('mt-12 flex flex-col items-center gap-8', hasActiveFilters && 'mt-8')}>
       <ActiveFilters filters={filters} />
       <ExcursionsLoading isPending={isPending} />
       {sortedExcursions.length > 0 ? (
