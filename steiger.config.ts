07acfd952592db3a6d45d82bbddc1df5288c33c9
JosephPerson <thejosephperson@mail.ru>
2026-02-09 18:45:14 +0300

    feat(fsd-steiger): configure fsd-steiger and add it to husky pre-commit

--- steiger.config.ts
+++ steiger.config.ts
@@ -2,4 +2,12 @@ import { defineConfig } from 'steiger';
 
 import fsd from '@feature-sliced/steiger-plugin';
 
-export default defineConfig([...fsd.configs.recommended, {}]);
+export default defineConfig([
+  ...fsd.configs.recommended,
+  {
+    files: ['./src/**'],
+    rules: {
+      'fsd/insignificant-slice': 'off',
+    },
+  },
+]);
