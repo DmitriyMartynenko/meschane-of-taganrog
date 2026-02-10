689b3f16d1b4bb13934c1d332356ac1052d9f4a8
JosephPerson <thejosephperson@mail.ru>
2026-02-10 19:00:44 +0300

    fix(header): increase z-index of Header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
   return (
     <header
       className={cn(
-        'fixed inset-x-0 top-0 z-1 w-full border-b transition-all duration-300 ease-in-out',
+        'fixed inset-x-0 top-0 z-50 w-full border-b transition-all duration-300 ease-in-out',
         isScrolled
           ? 'border-b-transparent bg-primary/75 shadow-lg backdrop-blur-lg'
           : 'border-b-muted-primary/35 bg-transparent'
