299f446c568fc7ef096561acb40383aa00daee66
JosephPerson <thejosephperson@mail.ru>
2026-02-17 20:28:50 +0300

    fix(prisma): update prisma client

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -2,10 +2,16 @@ import { PrismaPg } from '@prisma/adapter-pg';
 
 import { PrismaClient } from './generated/client';
 
+const connectionString = `${process.env.DATABASE_URL}`;
+
+if (!connectionString) {
+  throw new Error(`DATABASE_URL не найден. Проверьте файлы .env`);
+}
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
 });
 
 const prisma =
