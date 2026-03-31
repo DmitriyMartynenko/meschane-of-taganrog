979ae059af478b6b8acff69e33974da82ebc2f20
JosephPerson <thejosephperson@mail.ru>
2026-03-31 16:02:51 +0300

    style(booking-dialog): decrease gap for booking dialog on mobile

--- src/features/booking/ui/booking-dialog/BookingDialogMobile.tsx
+++ src/features/booking/ui/booking-dialog/BookingDialogMobile.tsx
@@ -27,7 +27,7 @@ export const BookingDialogMobile = (props: BookingDialogProps) => {
         showCloseButton={false}
         className="rounded-none border-t border-primary/30 bg-background-primary px-4 pt-6"
       >
-        <div className="flex flex-col items-center gap-8 border border-primary border-b-transparent px-6 py-8">
+        <div className="flex flex-col items-center gap-6 border border-primary border-b-transparent px-6 py-8">
           <SheetHeader className="gap-1 p-0 text-center">
             <Heading>
               <SheetTitle asChild>
@@ -43,10 +43,11 @@ export const BookingDialogMobile = (props: BookingDialogProps) => {
             </Heading>
           </SheetHeader>
           <BookingForm className="w-full max-w-lg" form={form} onSubmit={onSubmit} />
-          <SheetClose onClick={() => handleOpenChange(isOpen)} asChild>
+          <SheetClose asChild>
             <Button
-              className="p-0 text-destructive-primary enabled:active:text-destructive-primary/80"
+              className="p-0 text-muted-primary"
               variant="ghost"
+              onClick={() => handleOpenChange(isOpen)}
             >
               Отмена
             </Button>
