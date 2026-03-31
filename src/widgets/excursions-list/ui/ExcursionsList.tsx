89ac1fd66a5c28ad186441c9395a66a7284793f9
JosephPerson <thejosephperson@mail.ru>
2026-03-31 16:27:26 +0300

    style(excursions-list): add responvise gaps and Framer animations

--- src/widgets/excursions-list/ui/ExcursionsList.tsx
+++ src/widgets/excursions-list/ui/ExcursionsList.tsx
@@ -5,43 +5,47 @@ import { useExcursionFiltering, useFilterStore } from '@/features/excursion-filt
 
 import { ExcursionCard } from '@/entities/excursion';
 
-import { cn } from '@/shared/lib';
+import { cn, fadeUp, staggerContainer, viewport } from '@/shared/lib';
+import { MotionDiv } from '@/shared/ui';
 
 import { sortExcursionsByDate } from '../lib/sort-excursions-by-date';
 import { excursions } from '../model/excursions.mock';
 
 import { ActiveFilters } from './ActiveFilters';
-import { BackToFilters } from './BackToFilters';
 import { ExcursionsLoading } from './ExcursionsLoading';
 import { NoExcursionsPlaceholder } from './NoExcursionsPlaceholder';
 
 export const ExcursionsList = () => {
   const { filteredExcursions, isPending } = useExcursionFiltering(excursions);
   const filters = useFilterStore((state) => state.filters);
-  const hasActiveFilters = useFilterStore((state) => state.hasActiveFilters);
   const openBookingDialog = useBookingDialogStore((state) => state.open);
 
   const sortedExcursions = sortExcursionsByDate(filteredExcursions, 'asc');
 
   return (
-    <div
-      className={cn('relative mt-12 flex flex-col items-center gap-8', hasActiveFilters && 'mt-8')}
-    >
+    <div className={cn('relative mt-6 flex flex-col items-center gap-6 lg:mt-8 lg:gap-8')}>
       <ActiveFilters filters={filters} />
       <ExcursionsLoading isPending={isPending} />
       {sortedExcursions.length > 0 ? (
-        <div className="relative flex w-full flex-wrap items-stretch justify-center gap-8">
+        <MotionDiv
+          className="relative flex w-full flex-wrap items-stretch justify-center gap-8"
+          variants={staggerContainer(0.1)}
+          initial="hidden"
+          whileInView="visible"
+          viewport={viewport}
+        >
           {sortedExcursions.map((excursion) => (
-            <ExcursionCard
-              className="basis-1/3"
-              key={excursion.id}
-              excursion={excursion}
-              onStartBooking={openBookingDialog}
-            />
+            <MotionDiv key={excursion.id} variants={fadeUp} className="basis-1/3">
+              <ExcursionCard
+                className="h-full lg:min-w-112.5"
+                key={excursion.id}
+                excursion={excursion}
+                onStartBooking={openBookingDialog}
+              />
+            </MotionDiv>
           ))}
           <BookingDialog />
-          <BackToFilters />
-        </div>
+        </MotionDiv>
       ) : (
         <NoExcursionsPlaceholder />
       )}
