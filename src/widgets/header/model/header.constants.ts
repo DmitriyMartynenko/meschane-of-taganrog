6edf510af495fc46b1fef831927f897a0fe6e7e6
JosephPerson <thejosephperson@mail.ru>
2026-04-03 13:31:54 +0300

    refactor(header): add PAGES constant

--- src/widgets/header/model/header.constants.ts
+++ src/widgets/header/model/header.constants.ts
@@ -1,35 +1,35 @@
-import { SECTION_IDS } from '@/shared/model';
+import { PAGES, SECTION_IDS } from '@/shared/model';
 
 import { NavLink } from './header.types';
 
 export const NAV_LINKS: NavLink[] = [
   {
     label: 'Главная',
-    href: '/',
+    href: PAGES.HOME,
   },
   {
     label: 'О мещанах',
-    href: `/#${SECTION_IDS.HOME_ABOUT_MESHCHANE}`,
+    href: `${PAGES.HOME}#${SECTION_IDS.HOME_ABOUT_MESHCHANE}`,
   },
   {
     label: 'Особенности',
-    href: `/#${SECTION_IDS.HOME_FEATURES}`,
+    href: `${PAGES.HOME}#${SECTION_IDS.HOME_FEATURES}`,
   },
   {
     label: 'Экскурсии',
-    href: `/excursions`,
+    href: PAGES.EXCURSIONS,
   },
   {
     label: 'Фотографии',
-    href: `/#${SECTION_IDS.HOME_PHOTO}`,
+    href: `${PAGES.HOME}#${SECTION_IDS.HOME_PHOTO}`,
   },
   {
     label: 'ТГЛИАМЗ',
-    href: `/#${SECTION_IDS.HOME_PARTNERS}`,
+    href: `${PAGES.HOME}#${SECTION_IDS.HOME_PARTNERS}`,
   },
   {
     label: 'Контакты',
-    href: `/#${SECTION_IDS.SHARED_CONTACTS}`,
+    href: `${PAGES.HOME}#${SECTION_IDS.SHARED_CONTACTS}`,
   },
 ];
 
