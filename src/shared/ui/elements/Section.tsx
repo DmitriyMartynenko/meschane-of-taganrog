5ce15e549d1a238986c5b8a711d40c66096d501c
JosephPerson <thejosephperson@mail.ru>
2026-04-27 11:12:22 +0300

    refactor(section): remove noScrollMargin prop from Section component

--- src/shared/ui/elements/Section.tsx
+++ src/shared/ui/elements/Section.tsx
@@ -2,18 +2,11 @@ import { type ComponentProps } from 'react';
 
 import { cn } from '@/shared/lib';
 
-type SectionProps = {
-  noScrollMargin?: boolean;
-} & ComponentProps<'section'>;
-
-export const Section = (props: SectionProps) => {
-  const { className, children, noScrollMargin = false, ...restProps } = props;
+export const Section = (props: ComponentProps<'section'>) => {
+  const { className, children, ...restProps } = props;
 
   return (
-    <section
-      className={cn('w-full scroll-mt-[10vh]', noScrollMargin && 'scroll-mt-0', className)}
-      {...restProps}
-    >
+    <section className={cn('w-full scroll-mt-[10vh]', className)} {...restProps}>
       {children}
     </section>
   );
